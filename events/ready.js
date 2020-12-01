c37964a0b93c8897b2008f95484fe7163c749708
Glitch (ukqzneglence) <none>
2020-12-01 13:41:22 +0000

    🔋⛳️ Checkpoint ./events/ready.js:16929997/82

--- events/ready.js
+++ events/ready.js
@@ -11,7 +11,7 @@ module.exports = client => {
     `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: Aktif, Komutlar yüklendi!`
   );
   console.log(
-    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${//x_REDDAWN_x#0001
+    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${
       client.user.username
     } ismi ile giriş yapıldı!`
   );
@@ -22,10 +22,10 @@ module.exports = client => {
     "Woli Eğlence",
     "!yardım",
   ];
-//x_REDDAWN_x#0001
+
   setInterval(function() {
     var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
 
     client.user.setActivity(oyun[random], "WATCHING");
   }, 2 * 2500);
-};//x_REDDAWN_x#0001
+};
