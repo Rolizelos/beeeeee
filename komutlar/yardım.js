120295fefcd90372b95abc3f635c64f8a88dcb5c
Glitch (woli) <none>
2020-12-17 07:03:54 +0000

    Import of Rolizelos/guard

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,59 +1,45 @@
-const Discord = require ("discord.js");
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
 
-exports.run = (client, message) => {
+var prefix = ayarlar.prefix;
 
-const EmbedFwhyCode = new Discord.MessageEmbed()
+exports.run = async(client, message, args) => {
 
-.setColor("RANDOM")
-.setTitle("**▬▬▬▬▬▬[** ©️ **Eğlence Bot (V12)** ©️ **]▬▬▬▬▬▬**  \n\n> :floppy_disk: `!istatistik` **Botun istatistik bilgilerini açar.** \n> :floppy_disk: **Şuanda kullanılan prefix** `!`")
-.setThumbnail("https://cdn.discordapp.com/attachments/655459488236568597/655887650494087178/loading_1.gif")
-.setDescription(`
+        const CML = new Discord.MessageEmbed()
 
-▬▬▬▬▬▬[ :closed_lock_with_key: **Eğlence Komutları** :closed_lock_with_key: ]▬▬▬▬▬▬
+             .setColor('#fff000')
+             .addField(`İşte Komutlarım!`, `
+             ${prefix}antiraid aç
+             ${prefix}capsengel aç
+             ${prefix}emojikoruma aç
+             ${prefix}everengel aç
+             ${prefix}kanalkoruma aç
+             ${prefix}küfürengel aç
+             ${prefix}modlog #kanal
+             ${prefix}reklamengel aç
+             ${prefix}rolkoruma aç
+             ${prefix}sohbet-aç
+             ${prefix}sohbet-kapat
+             ${prefix}sil-üye @üye miktar
+             ${prefix}sil miktar
+             ${prefix}yavaşmod süre
+             ${prefix}ban @üye sebep
+             ${prefix}kick @üye sebep             
+             `)
+             .setFooter(`${message.author.username} Tarafından istendi.`, message.author.avatarURL())
+            
+        return message.channel.send(CML);
+}
 
-> **» !aduketçek :** Etiketlediğiniz kişiye aduket çekersiniz.
-> **» !balık-tut :** Denizde balık tutarsınız.
-> **» !beşlik :** Etiketlediğiniz kişiyle beşlik çakarsınız.
-> **» !ara155 :** Polisi aramanıza yarar.
-> **» !efkarım :** Şu anda olan efkarınızı ölçersiniz.
-> **» !ejderha-yazı :** Ejderha temalı logo üretirsiniz.
-> **» !espri :** Bot sizin için espri yapar.
-> **» !hesapla :** Belirttiğiniz matematik işlemini bot yapar.
-> **» !kartopu :** Etiketlediğiniz kişiye kartopu atarsınız.
-> **» !kralol :** Kral olmanıza yarar.
-> **» !tokat :** Etiketlediğiniz kişiyi tokatlarsınız.
-> **» !yazıtura :** Bot ile yazı-tura oyununu oynarsınız.
-> **» !yılbaşı :** Yılbaşının gelmesine ne kadar yaklaştığınızı gösterir.
-> **» !öp :** Etiketlediğiniz kişiyi öpmenize yarar.
-> **» !şekerye :** Bot size şeker ikram eder.
-
-**▬▬▬▬▬▬▬[** :gear: **Bilgilendirme** :gear: **]▬▬▬▬▬▬▬**
-
-> :dizzy: **Fikirleriniz** **değerlidir, Belirtmekten asla çekinmeyin!**
-> :open_file_folder: **Botun Destek Sunucusuna Gelmek İçin [Tıkla!](https://discord.gg/davetlinkiniz)**
-> :round_pushpin: **Komut hakkında detaylı bilgi için: !yardım**
-> :airplane: **Aktif discord.js sürümüm: v12.2.0**
-
-**» Bağlantılar** 
-**[Destek Sunucusu](https://discord.gg/davetlinkiniz)** **•** **[Botun Davet Linki](https://www.discord.com/)** **•** **[Web-Site](https://www.discord.com/)**
-`)
- 
-.setFooter(client.user.username + "", client.user.avatarURL)
-.setTimestamp();
-
-return message.channel.send(EmbedFwhyCode)
-.then; //FwhyCode
-
-};
 exports.conf = {
-    enabled: true, 
-    guildOnly: false, 
-    aliases: [], 
-    permLevel: 0 
-};
-  
-  exports.help = {
-    name: 'yardım', 
-    description: 'The Help Command',
-    usage: 'help'
-};
+	enabled : true,
+	guildOnly : false,
+	aliases : ['help'],
+	permLevel : 0
+}
+exports.help = {
+	name : 'yardım',
+	description : 'Komut kategorilerini atar',
+	usage : '!yardım'
+}
+//DÜZENLENECEK
