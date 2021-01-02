ff55172e37ead056a5e081c5a1d1e4c5a7745945
Glitch (bee6bot) <none>
2021-01-02 14:06:05 +0000

    🏀🍰 Checkpoint ./komutlar/ban-list.js:16929997/2534 ./komutlar/server-bilgi.js:16929997/1473 ./komutlar/sohbet.js:16929997/1992 ./komutlar/otorol-ayarla.js:16929997/1927 ./komutlar/otorol.js:16929997/1126 ./index.js:16929997/1125 ./komutlar/otorol-kapat.js:16929997/1371 ./otorol.json:16929997/2240

--- index.js
+++ index.js
@@ -189,3 +189,36 @@ client.on("message", async msg => {
     if (!i) return;
  
     });
+//otorol
+client.on("guildMemberAdd", async member => {
+  let sayac = JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
+let otorole =  JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
+let arole = otorole[member.guild.id].sayi
+let giriscikis = JSON.parse(fs.readFileSync("./otorol.json", "utf8"));  
+let embed = new Discord.MessageEmbed()
+.setTitle('Otorol Sistemi')
+.setDescription(`:inbox_tray: @${member.user.tag}'a Otorol Verildi `)
+.setColor("RANDOM")
+.setFooter("Hollywood", client.user.avatarURL);
+
+if (!giriscikis[member.guild.id].kanal) {
+return;
+}
+
+try {
+let giriscikiskanalID = giriscikis[member.guild.id].kanal;
+let giriscikiskanali = client.guilds.get(member.guild.id).channels.get(giriscikiskanalID);
+giriscikiskanali.send(`:inbox_tray:   Hoşgeldin **${member.user.tag}** Rolün Başarıyla Verildi.`);
+} catch (e) { // eğer hata olursa bu hatayı öğrenmek için hatayı konsola gönderelim.
+return console.log(e)
+}
+
+});
+
+client.on("guildMemberAdd", async (member) => {
+let autorole =  JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
+let role = autorole[member.guild.id].sayi
+
+member.addRole(role)
+
+});
