4f2152134acbc82649cc71c6f5b3eb2620488c56
Glitch (bee6bot) <none>
2021-01-02 12:55:51 +0000

    🍁🍨 Checkpoint ./komutlar/yardım.js:16929997/1702 ./komutlar/bot-bilgi.js:16929997/3366 ./index.js:16929997/6

---
+++ komutlar/yardım.js
@@ -0,0 +1,43 @@
+const Discord = require ("discord.js");
+
+exports.run = (client, message) => {
+
+const EmbedFwhyCode = new Discord.MessageEmbed()
+
+.setColor("RANDOM")
+.setTitle("**▬▬▬▬▬▬[** ©️ **Bee6 Bot (V12)** ©️ **]▬▬▬▬▬▬**  \n\n> :floppy_disk: `b!istatistik` **Botun istatistik bilgilerini açar.** \n> :floppy_disk: **Şuanda kullanılan prefix** `b!`")
+.setThumbnail('https://cdn.discordapp.com/attachments/778643898272055337/794908648186314752/tenor.gif')
+.setDescription(`
+
+▬▬▬▬▬▬[ :bee: **Bot Komutlarıı** :bee: ]▬▬▬▬▬▬
+
+> **» b!kayıt-yardım :** Kayıt Komutlarını Gösterir.
+> **» b! :** .
+> **» b!... :** ...
+> **» b!... :** ...
+
+
+**▬▬▬▬▬▬▬[** :gear: **Bilgilendirme** :gear: **]▬▬▬▬▬▬▬**
+Bu komut ${message.author.username} tarafından istendi! 
+
+`)
+ 
+.setFooter(client.user.username + "", client.user.avatarURL)
+.setTimestamp();
+
+return message.channel.send(EmbedFwhyCode)
+.then;
+
+};
+exports.conf = {
+    enabled: true, 
+    guildOnly: false, 
+    aliases: [], 
+    permLevel: 0 
+};
+  
+  exports.help = {
+    name: 'yardım', 
+    description: 'The Help Command',
+    usage: 'yardım'
+};
