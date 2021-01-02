3f711e02ce231d9173b0c389bf77208be0489875
Glitch (bee6bot) <none>
2021-01-02 10:43:00 +0000

    👦🕵 Checkpoint ./komutlar/istatistik.js:16929997/43 ./komutlar/sa-as.js:16929997/950 ./events/ready.js:16929997/1316 ./index.js:16929997/1155

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,33 @@
-const Discord = require('discord.js');
-const prefix = process.env.PREFIX;
+
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
+
+var prefix = ayarlar.prefix;
 
 module.exports = client => {
-client.user.setActivity('b!yardım | v100 | #FaoX - #FunsT', {type: 'WATCHING'}); 
+  console.log(
+    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: Aktif, Komutlar yüklendi!`
+  );
+  console.log(
+    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${
+      client.user.username
+    } ismi ile giriş yapıldı!`
+  );
+  client.user.setStatus("");
+  var oyun = [
+    "#BAKIMDA...",
+    "v100",
+    "#BAKIMDA...",
+    "#FaoX - #FunsT",
+    "#BAKIMDA..."
+    
+  ];
 
-}
+  setInterval(function() {
+    var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
+
+    client.user.setActivity(oyun[random], ""); 
+  }, 2 * 2500);
+};
